bd3a0ba25358b0df95a99c96f59d0a14aa9c705c
DanielSwieca <danielswieca@gmail.com>
2020-12-02 20:04:29 -0500

    Gesture Recognizer Part 1

---
+++ QuestFirstSetup/Assets/Scripts/MovementRecognizer.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class MovementRecognizer : MonoBehaviour
+{
+    public XRNode inputSource;
+    public InputHelpers.Button inputButton;
+    public float inputThreshold = 0.1f;
+    public Transform movementSource;
+
+    public float newPositionThresholdDistance = 0.05f;
+    public GameObject debugCubePrefab;
+
+    private bool isMoving = false;
+    private List<Vector3> positionsList = new List<Vector3>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputHelpers.IsPressed(InputDevices.GetDeviceAtXRNode(inputSource), inputButton, out bool isPressed, inputThreshold);
+
+        //Start the movement
+        if(!isMoving && isPressed)
+        {
+            StartMovement();
+        } 
+        //Ending the movement
+        else if(isMoving && !isPressed)
+        {
+            EndMovement();
+        } 
+        //Updating the movement
+        else if(isMoving && isPressed)
+        {
+            UpdateMovement();
+        }
+    }
+
+    void StartMovement()
+    {
+        Debug.Log("Start Movement");
+        isMoving = true;
+        positionsList.Clear();
+        positionsList.Add(movementSource.position);
+
+        if (debugCubePrefab)
+        {
+           Destroy( Instantiate(debugCubePrefab, movementSource.position, Quaternion.identity) , 3);
+        }
+    }
+
+    void EndMovement()
+    {
+        Debug.Log("End Movement");
+        isMoving = false;
+    }
+
+    void UpdateMovement()
+    {
+        Debug.Log("Update Movement");
+        Vector3 lastPosition = positionsList[positionsList.Count - 1];
+        if(Vector3.Distance(movementSource.position, lastPosition) > newPositionThresholdDistance)
+        {
+            positionsList.Add(movementSource.position);
+
+            if (debugCubePrefab)
+            {
+                Destroy(Instantiate(debugCubePrefab, movementSource.position, Quaternion.identity), 3);
+            }
+        }
+
+    }
+}
