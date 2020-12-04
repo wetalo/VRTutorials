b1825125b43ac504094c376ad8cfa41bc6328bd0
DanielSwieca <danielswieca@gmail.com>
2020-12-03 22:07:24 -0500

    Gesture recognition Part 2

--- QuestFirstSetup/Assets/Scripts/MovementRecognizer.cs
+++ QuestFirstSetup/Assets/Scripts/MovementRecognizer.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using PDollarGestureRecognizer;
+using System.IO;
+using UnityEngine.Events;
 
 public class MovementRecognizer : MonoBehaviour
 {
@@ -12,7 +15,16 @@ public class MovementRecognizer : MonoBehaviour
 
     public float newPositionThresholdDistance = 0.05f;
     public GameObject debugCubePrefab;
+    public bool creationMode = true;
+    public string newGestureName;
 
+    public float recognitionThreshold = 0.8f;
+
+    [System.Serializable]
+    public class UnityStringEvent : UnityEvent<string> { }
+    public UnityStringEvent OnRecognized;
+
+    private List<Gesture> traningSet = new List<Gesture>();
     private bool isMoving = false;
     private List<Vector3> positionsList = new List<Vector3>();
 
@@ -20,7 +32,11 @@ public class MovementRecognizer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        string[] gestureFiles = Directory.GetFiles(Application.persistentDataPath, "*.xml");
+        foreach (var item in gestureFiles)
+        {
+            traningSet.Add(GestureIO.ReadGestureFromFile(item));
+        }
     }
 
     // Update is called once per frame
@@ -62,6 +78,34 @@ public class MovementRecognizer : MonoBehaviour
     {
         Debug.Log("End Movement");
         isMoving = false;
+
+        //Create the gesture from the position list
+        Point[] pointArray = new Point[positionsList.Count];
+        for (int i = 0; i < positionsList.Count; i++)
+        {
+            Vector2 screenPoint = Camera.main.WorldToScreenPoint(positionsList[i]);
+            pointArray[i] = new Point(screenPoint.x, screenPoint.y, 0);
+        }
+
+        Gesture newGesture = new Gesture(pointArray);
+
+        if (creationMode)
+        {
+            newGesture.Name = newGestureName;
+            traningSet.Add(newGesture);
+
+            string fileName = Application.persistentDataPath + "/" + newGestureName + ".xml";
+            GestureIO.WriteGesture(pointArray, newGestureName, fileName);
+        } else
+        {
+            Result result = PointCloudRecognizer.Classify(newGesture, traningSet.ToArray());
+            Debug.Log(result.GestureClass + result.Score);
+
+            if(result.Score > recognitionThreshold)
+            {
+                OnRecognized.Invoke(result.GestureClass);
+            }
+        }
     }
 
     void UpdateMovement()
---
+++ QuestFirstSetup/Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public List<GameObject> objects;
+
+    public void Spawn(string objectName)
+    {
+        Debug.Log("Spawn: " + objectName);
+        foreach (var item in objects)
+        {
+            item.SetActive(objectName == item.name);
+        }
+    }
+}
